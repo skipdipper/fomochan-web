f7a127486363d2fb4c8ef7c2ec881dd8f32bb93c
skipdipper <morgan-yyh@protonmail.com>
2022-08-26 09:10:24 +0000

    Refactor Catalog Page to use SSR instead of CSR

--- src/common/components/ThreadList.js
+++ src/common/components/ThreadList.js
@@ -1,32 +1,54 @@
 import Post from './Post'
+import Card from './Card'
 
-export default function ThreadList({ data }) {
-    const threads = data.map((thread) =>
-        <div className='thread' key={thread.post_id}>
-            <Post
-                subject={thread.subject}
-                name={thread.name}
-                created_at={thread.created_at}
-                post_id={thread.post_id}
-                comment={thread.comment}
-                filesize={thread.filesize}
-                filename={thread.filename}
-                ext={thread.ext}
-                width={thread.width}
-                height={thread.height}
-                thumbnailWidth={thread.thumbnail_w}
-                thumbnailHeight={thread.thumbnail_h}
-                thread_id={thread.thread_id}
-                replies={thread.replies}
-                images={thread.images}
-                tim={thread.tim}
-                op={true}
-            />
-        </div>
-    )
+
+export default function ThreadList({ data, catalog = false }) {
+    const threads = catalog
+        ? data.map((thread) =>
+            <div className='thread' key={thread.post_id}>
+                <Card
+                    subject={thread.subject}
+                    name={thread.name}
+                    created_at={thread.created_at}
+                    post_id={thread.post_id}
+                    comment={thread.comment}
+                    filesize={thread.filesize}
+                    filename={thread.filename}
+                    width={thread.width}
+                    height={thread.height}
+                    thread_id={thread.thread_id}
+                    replies={thread.replies}
+                    images={thread.images}
+                    tim={thread.tim}
+                />
+            </div>
+        )
+        : data.map((thread) =>
+            <div className='thread' key={thread.post_id}>
+                <Post
+                    subject={thread.subject}
+                    name={thread.name}
+                    created_at={thread.created_at}
+                    post_id={thread.post_id}
+                    comment={thread.comment}
+                    filesize={thread.filesize}
+                    filename={thread.filename}
+                    ext={thread.ext}
+                    width={thread.width}
+                    height={thread.height}
+                    thumbnailWidth={thread.thumbnail_w}
+                    thumbnailHeight={thread.thumbnail_h}
+                    thread_id={thread.thread_id}
+                    replies={thread.replies}
+                    images={thread.images}
+                    tim={thread.tim}
+                    op={true}
+                />
+            </div>
+        )
 
     return (
-        <div className='board'>
+        <div className={catalog ? 'catalog' : 'board'}>
             {threads}
         </div>
     )
--- src/pages/[board]/catalog.js
+++ src/pages/[board]/catalog.js
@@ -1,59 +1,41 @@
-import Card from '../../common/components/Card'
+import Error from 'next/error'
 import { useState, useEffect } from 'react';
+
 import useFetch from '../../common/hooks/useFetch';
+import Card from '../../common/components/Card'
+import ThreadList from '../../common/components/ThreadList';
 
 
-// hack pass parent state as prop
-function Threads() {
-    const {data, isLoading, error} = useFetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/a/threads`, {});
+export default function Catalog({ errorCode, data: threads, board }) {
 
-    // call Main scroll position hook
-    // useMaintainScroll();
-
-    if (isLoading) return <p>Loading...</p>
-    if (error) return <p>Unexpected server error</p>
-    if (!data) return <p>Board has no threads</p>
-
-    const threads = data.map((thread) =>
-        <div className='catalog-item' key={thread.post_id}>
-            <Card
-                subject={thread.subject}
-                name={thread.name}
-                created_at={thread.created_at}
-                post_id={thread.post_id}
-                comment={thread.comment}
-                filesize={thread.filesize}
-                filename={thread.filename}
-                width={thread.width}
-                height={thread.height}
-                thread_id={thread.thread_id}
-                replies={thread.replies}
-                images={thread.images}
-                tim={thread.tim}
-                // op={true}
-
-            />
-        </div>
-    )
-
-    return (
-        <div className='catalog'>
-            {threads}
-            {console.log('finished rendering threads')}
-        </div>
-    )
-
-}
-
-export default function Catalog({ url }) {
-
-    // const router = useRouter();
-    // const { threadNo } = router.query;
+    if (errorCode) {
+        return <Error statusCode={errorCode} />
+    }
 
     return (
         <>
             <h1>Catalog</h1>
-            <Threads/>
+            <ThreadList data={threads} catalog={true} />
         </>
     )
+}
+
+
+export async function getServerSideProps(context) {
+    const { board } = context.params;
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/${board}/threads`);
+    const errorCode = res.ok ? false : res.status
+    const data = await res.json();
+    console.log(data);
+    console.log(`getServerSideProps for Catalog Board /${board}`);
+    if (!data) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: { errorCode, data, board },
+    }
 }
